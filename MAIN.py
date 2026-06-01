04dfd11fc494348bf83fe7465dbe613d7f80cbc6
lukehalcrow1 <22156@nayland.school.nz>
2026-06-02 10:23:59 +1200

    added changes like no resize and cant submit without boxes fill up

--- MAIN.py
+++ MAIN.py
@@ -1,7 +1,17 @@
-import tkinter as tk
-from tkinter import messagebox
+rom tkinter import messagebox
 
 data_dict = {}
+
+def check_fields(*args): 
+    all_filled = (
+        entry1.get().strip() != "" and
+        entry2.get().strip() != "" and
+        entry3.get().strip() != ""
+    )
+    if all_filled:
+        save_button.config(state="normal", bg="green")
+    else:
+        save_button.config(state="disabled", bg="grey")
 
 def save_data():
     key = entry1.get()
@@ -24,20 +34,29 @@ def save_data():
 root = tk.Tk()
 root.title("issue")
 root.geometry("400x250")
+root.resizable(False, False)  
+
+var1 = tk.StringVar()
+var2 = tk.StringVar()
+var3 = tk.StringVar()
+var1.trace("w", check_fields)  
+var2.trace("w", check_fields)
+var3.trace("w", check_fields)
 
 tk.Label(root, text="issue").pack(pady=5)
-entry1 = tk.Entry(root, width=40)
+entry1 = tk.Entry(root, width=40, textvariable=var1) 
 entry1.pack()
 
 tk.Label(root, text="issue").pack(pady=5)
-entry2 = tk.Entry(root, width=40)
+entry2 = tk.Entry(root, width=40, textvariable=var2) 
 entry2.pack()
 
 tk.Label(root, text="where").pack(pady=5)
-entry3 = tk.Entry(root, width=40)
+entry3 = tk.Entry(root, width=40, textvariable=var3)  
 entry3.pack()
 
-save_button = tk.Button(root, text="submit", command=save_data)
+save_button = tk.Button(root, text="submit", command=save_data,
+    state="disabled", bg="grey") 
 save_button.pack(pady=20)
 
 root.mainloop()
